d7327d318cd0ed1e0ea741023d08e989a77c88fa
nikhil7127 <59138968+nikhil7127@users.noreply.github.com>
2020-07-14 19:25:17 +0530

    updated

--- Main_file.py
+++ Main_file.py
@@ -1,5 +1,4 @@
 import pandas as p 
-import time as t
 import os
 i=0
 j=0
@@ -10,24 +9,22 @@ tot_sbi1 =0
 tot_other1 =0
 
 files = input("Enter file name(only excel files) : ")
+date  = input("Enter date(dd-mm-year)")
 try:
-    os.remove("SBI_ACCOUNTS.txt")
-    os.remove("OTHER_ACCOUNTS.txt")
-    os.remove("TOTAL.txt")
+    os.remove(f"SBI_ACCOUNTS_{date}.txt")
+    os.remove(f"OTHER_ACCOUNTS_{date}.txt")
+    os.remove(f"TOTAL_{date}.txt")
 except:
     pass
-print(t.clock())
 k = p.read_excel(f"{files}.xlsx")
-#k = p.read_excel(f"another.xlsx")
 lens = len(k)
 for a in range(0,lens):
     let = k['BNK_IFSC'][a].split("0")
     let = let[0]
     if(let == "SBIN"):
         if(i==0):
-            with open("SBI_ACCOUNTS.txt","a") as n:
-                n.write("""
-APEPDCL - Sri V S RAMACHANDRA MURTHY
+            with open(f"SBI_ACCOUNTS_{date}.txt","a") as n:
+                n.write("""APEPDCL - Sri V S RAMACHANDRA MURTHY
 EE O BHIMAVARAM
 TRANSACTIONS ANDHRA BANK - 04/07/2020 - EMPLOYEES
 --------------------------------------------------------------------------------
@@ -37,7 +34,7 @@ Sl.    IFSC CODE         Account           Amount          Name
         ask = k['CR_AMOUNT'][a]
         tot_sbi = tot_sbi+1
         tot_sbi1 = tot_sbi1+ask
-        with open("SBI_ACCOUNTS.txt","a") as n:
+        with open(f"SBI_ACCOUNTS_{date}.txt","a") as n:
     
             ask = str(ask)[::-1]
             final = ""
@@ -57,9 +54,8 @@ Sl.    IFSC CODE         Account           Amount          Name
        
     else:
         if(j==0):
-            with open("OTHER_ACCOUNTS.txt","a") as o:
-                o.write("""
-APEPDCL - Sri V S RAMACHANDRA MURTHY
+            with open(f"OTHER_ACCOUNTS_{date}.txt","a") as o:
+                o.write("""APEPDCL - Sri V S RAMACHANDRA MURTHY
 EE O BHIMAVARAM
 TRANSACTIONS ANDHRA BANK - 04/07/2020 - EMPLOYEES
 --------------------------------------------------------------------------------
@@ -69,7 +65,7 @@ Sl.    IFSC CODE         Account           Amount          Name
         ask1 = k['CR_AMOUNT'][a]
         tot_other = tot_other+1
         tot_other1 = tot_other1+ask1
-        with open("OTHER_ACCOUNTS.txt","a") as o:
+        with open(f"OTHER_ACCOUNTS_{date}.txt","a") as o:
             ask1 = str(ask1)[::-1]
             final1 = ""
             for aa1 in range(0,8):
@@ -86,34 +82,48 @@ Sl.    IFSC CODE         Account           Amount          Name
                 o.write(f"\t {k['EMP_NAME'][a]}\n")
             o.close()
 
-with open("SBI_ACCOUNTS.txt","a") as n:
+with open(f"SBI_ACCOUNTS_{date}.txt","a") as n:
     n.write("--------------------------------------------------------------------------------\n")
     n.write(f"                            Total         {tot_sbi1}.00                            \n")
     n.write("--------------------------------------------------------------------------------\n")
     n.close()
-with open("OTHER_ACCOUNTS.txt","a") as o:
+with open(f"OTHER_ACCOUNTS_{date}.txt","a") as o:
     o.write(f"--------------------------------------------------------------------------------\n")
     o.write(f"                            Total         {tot_other1}.00                           \n")
     o.write("--------------------------------------------------------------------------------\n")
     o.close()
 if(z==0):
-    with open("TOTAL.txt","a") as t:
-        t.write("""
-APEPDCL - Sri V S RAMACHANDRA MURTHY
+    with open(f"TOTAL_{date}.txt","a") as t:
+        t.write("""APEPDCL - Sri V S RAMACHANDRA MURTHY
 EE O BHIMAVARAM
-    SUMMARY OF TRANSACTIONS - 04/07/2020 - EMPLOYEES
+SUMMARY OF TRANSACTIONS - 04/07/2020 - EMPLOYEES
 --------------------------------------------------------------------------------
- BANK      NUMBER        AMOUNT         FILES GENERATED
+ BANK\t\tNUMBER\t\tAMOUNT\t\tFILES GENERATED
 --------------------------------------------------------------------------------\n""")
-        
-        t.write(f" SBI         {tot_sbi}          {tot_sbi1}.00")
-        t.write("\tSBI_ACCOUNTS.txt")
+        tot_c = str(tot_sbi1)[::-1]
+        tot_c1 = str(tot_other1)[::-1]
+        tot_count =""
+        tot_count1 =""
+        for asa in range(0,10):
+            if(asa>= len(tot_c)):
+                tot_count = tot_count+" "
+            else:
+                tot_count = tot_count+tot_c[asa]
+        for asa1 in range(0,10):
+            if(asa1>= len(tot_c1)):
+                tot_count1 = tot_count1+" "
+            else:
+                tot_count1 = tot_count1+tot_c1[asa1]
+        tot_count = tot_count[::-1]
+        tot_count1 = tot_count1[::-1]
+        t.write(f" SBI\t\t{tot_sbi}\t{tot_count}.00")
+        t.write("\t\tSBI_ACCOUNTS.txt")
         t.write("\n")
         t.write("\n")
-        t.write(f" OTHER       {tot_other}          {tot_other1}.00")
-        t.write("\t        OTHER_ACCOUNTS.txt")
+        t.write(f" OTHER\t\t{tot_other}\t{tot_count1}.00")
+        t.write("\t\tOTHER_ACCOUNTS.txt")
         t.write("\n")
         t.write(f"""--------------------------------------------------------------------------------
- TOTAL       {tot_sbi+tot_other} \t {tot_sbi1+tot_other1}.00
+ TOTAL\t\t{tot_sbi+tot_other}\t   {tot_sbi1+tot_other1}.00
 --------------------------------------------------------------------------------""")
-print(tot_sbi1,tot_other1)
+
